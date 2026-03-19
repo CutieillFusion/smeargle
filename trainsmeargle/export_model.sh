9e03210a181ee63f678e4108484451705ba03f9a
Dylan Norquist <norquistdylan@gmail.com>
2026-03-19 01:05:14 -0500

    nuking unneeded stuff

--- trainsmeargle/export_model.sh
+++
@@ -1,7 +0,0 @@
-config_path="config.json"
-model_path=$1
-save_dir=$2
-
-mkdir -p ../models/$save_dir
-cp $config_path ../models/$save_dir/config.json
-cp models/$model_path/best_model/pytorch_model.bin ../models/$save_dir/pytorch_model.bin
